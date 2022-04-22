e9086308bac4072d446f4234d3d320b9bca9541b
H4zz4rd <h4zz4rd@c64gamer.com>
2022-04-22 11:54:52 +0200

    Route Fix #2

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -19,8 +19,6 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
-use H4zz4rdDev\Seat\SeatBuyback\Http\Controllers\BuybackContractController;
-
 Route::group([
     'namespace' => 'H4zz4rdDev\Seat\SeatBuyback\Http\Controllers',
     'middleware' => ['web', 'auth', 'locale'],
