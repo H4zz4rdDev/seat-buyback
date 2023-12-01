733bd2783aa71a382e6b05a26d36e1c62e326c70
H4zz4rdDev <h4zz4rd@c64gamer.com>
2023-12-01 23:48:18 +0100

    Removing EveMarketer config values

--- src/Config/PriceProvider/buyback.priceProvider.php
+++ src/Config/PriceProvider/buyback.priceProvider.php
@@ -1,10 +1,6 @@
 <?php
 
 return [
-    'eveMarketer' => [
-        'name' => 'EVEMarketer',
-        'apiUrl' => 'https://api.evemarketer.com/ec/marketstat/json'
-    ],
     'evePraisal' => [
         'name' => 'EVEPraisal',
         'apiUrl' => 'https://evepraisal.com'
